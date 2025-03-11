e33e8bba21a466e708986aa649767fd93f4abb0b
Ahnaf Shahriyar Chowdhury <ahnafshahriyarchow@gmail.com>
2025-03-10 20:30:10 -0400

    Add files via upload

---
+++ task_5.py
@@ -0,0 +1,33 @@
+"""Consider the problem of minimum cost 3-coloring of the line graphs. Note that, in line graphs, nodes are ordered on a line and there can be edges only between two adjacent
+nodes. In a valid coloring, no adjacent nodes can have the same color. Implement an enumeration-based approach for this problem, which involves enumerating all the feasible combinations,
+calculating the cost associated with each combination, and outputting the minimum cost combination"""
+
+import itertools
+
+def min_cost_vertex_coloring_enumeration(costs):
+    no_of_vertices = len(costs)
+    best_coloring = None
+    min_cost = float('inf')
+
+    color_combinations = itertools.product([0, 1, 2], repeat=no_of_vertices)
+
+    for color in color_combinations:
+        valid = True
+        for i in range(1, no_of_vertices):
+            if color[i] == color[i - 1]:
+                valid = False
+                break
+        
+        if valid:
+            cost = sum(costs[i][color[i]] for i in range(no_of_vertices))
+            
+            if cost < min_cost:
+                min_cost = cost
+                best_coloring = color
+    
+    return min_cost, list(best_coloring)
+
+costs = [[18, 3, 18], [14, 14, 4], [15, 3, 17]]
+min_cost_enum, best_coloring_enum = min_cost_vertex_coloring_enumeration(costs)
+print(f"min_cost_enum: {min_cost_enum}") 
+print(f"best_coloring_enum: {(best_coloring_enum)}")
