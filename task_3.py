7b181c7e4d219d9ead81348124745453998a018b
Ahnaf Shahriyar Chowdhury <ahnafshahriyarchow@gmail.com>
2025-03-12 21:06:40 -0400

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -4,27 +4,30 @@ node cover for any given undirected graph. You can assume that you will be given
 an undirected graph, which shows whether there is an edge between any two nodes in the graph. Since this is a greedy algorithm, it may not generate the optimum (i.e., minimum-sized) node-cover, however, any
 generated solution by the greedy algorithm is still expected to be a node-cover."""
 
-
-
 def GetNodeCover_greedy(adj_matrix):
     n=len(adj_matrix)  
     nodes_covered=set()  
     edges=set()  
     
+    #extracting all edges from the matrix
     for i in range(n):
-        for j in range(i+1,n):
+        for j in range(i+1,n):  #consider only upper triangle to avoid duplicate edges
             if adj_matrix[i][j] == 1:
                 edges.add((i,j))
 
+    #greedy approach by selecting edges and add their endpoints to the cover
     while edges:
-        edge=edges.pop()
-        u,v=edge
+        edge=edges.pop()   #select a random edge from the set
+        u,v=edge          #extract the two nodes connected by that randomly selected edge
         nodes_covered.add(u)
         nodes_covered.add(v)
+
+        #remove all edges that are now covered by either u or v
         edges={e for e in edges if u not in e and v not in e}
         
     return list(nodes_covered)
 
+#Test case
 np_adjacent=[
     [0, 1, 0, 1, 0],
     [1, 0, 1, 1, 0],
